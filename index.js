86d0be61478c6d8130d14bb8c82afb3ed1ae2243
Sarah <sarah.liess@hotmail.de>
2023-02-09 14:32:04 +0100

    added dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const db = require("./db");
 const postsRoutes = require("./routes/posts");
 const authRoutes = require("./routes/auth");
